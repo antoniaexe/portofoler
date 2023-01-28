023d0dd2a99613625805b9592aae76cd81cb9441
Antonia Exertzi <antoniaexe@gmail.com>
2023-01-28 17:58:47 +0200

    Enable eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ]
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error']
+  }
+}
