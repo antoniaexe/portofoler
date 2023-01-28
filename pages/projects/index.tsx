0efccec76af1dd40c770e33b9d60e6967c32b908
Antonia Exertzi <antoniaexe@gmail.com>
2023-01-28 18:02:46 +0200

    Add project index and [id] pages

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,4 +1,48 @@
-const Projects = ()=>{
-  return "Γεια σας"
-}
-export default Projects
+import { Layout } from '../../components/Layout';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const Projects = () => {
+  const [projects, setProjects] = useState([]); // αυτο θα το βρισκει κανοντας ενα api call στο server στη βαση δεδομενων
+  useEffect(() => {
+    fetch('/api/projects', {
+      method: 'GET',
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  return (
+    <Layout>
+      <div>
+        <h1>Τα στουντιό σας:</h1>
+        {projects.length !== 0 ? (
+          <ul>
+            {projects.map((project) => {
+              return (
+                <div>
+                  <li>
+                    
+                    <Link href={`/projects/${project.name}`}>
+                      {project.name}
+                    </Link>
+                  </li>
+                </div>
+              );
+            })}
+          </ul>
+        ) : (
+          <p>Δεν εχετε studio. Πατηστε το κουμπι `δημιουργια νεου studio`</p>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default Projects;
