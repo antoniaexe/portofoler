a7a1a2bfe8ce459d274c8542a17a70c544dff529
Antonia Exertzi <antoniaexe@gmail.com>
2023-02-20 19:11:09 +0200

    Delete unused components and hooks files

--- components/ProjectEntry.tsx
+++
@@ -1,8 +0,0 @@
-export const ProjectEntry = ({href,title,photo}:{href: string, title: string, photo:string}) => {
-  return (
-    <a href={href}>{title}
-      <img src={photo}/>
-    </a>
-  );
-};
-export default ProjectEntry;
